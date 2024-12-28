f1e5a0b282dfa8f8f5f9eed47579fc0a55369268
Umer Qureshi <133916069+umerqureshi409@users.noreply.github.com>
2024-12-28 21:54:24 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,19 @@ import "./globals.scss";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import { Providers } from "@/components/Provider";
 import Alert from "@/components/UI/Alert";
-
 export const metadata = {
 	title: "Al - Quran",
+	icons: {
+		icon: [
+			{ url: '/Quran.png' },
+			{ url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+			{ url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+		],
+		apple: [
+			{ url: '/Quran.png' },
+		],
+	},
 };
-
 export default function RootLayout({
 	children,
 }: {
